9093155e74bfd76e3d1eed77d358b0c3e1519ee7
Fran Madueño <47178991+franmadu6@users.noreply.github.com>
2019-03-26 10:39:55 +0100

    ej2

--- movies.py
+++ movies.py
@@ -13,6 +13,22 @@ def listar_info(datos):
         info = info + "Title: " + str(lista[listapelis]) + "\nAño: " + str(lista[listapelis + 1]) + "\nDuracion: " + str(lista[listapelis + 2]).replace("PT","").replace("M"," Min.") + "\n\n"
     return info
 
+def contar_info(datos):
+    lista = []
+    for actor in range(0,99):
+        lista.append(datos[actor]["title"])
+        
+        lista_actor = []
+        for actor in datos[actor]["actors"]:
+            lista_actor.append(actor)
+        lista.append(len(lista_actor))
+    cad = "--Número de actores en cada pelicula: --\n\n"
+    
+    for actor in range(0,len(lista),2):
+        cad = cad + str(lista[actor]) + " tiene " + str(lista[actor + 1]) + " actores/actrices. " + "\n\n"
+    return cad
+
+    
 with open("movies.json","r") as fichero:
     datos = json.load(fichero)
 
@@ -20,6 +36,7 @@ print ("\n1. Listar información: Listar el título, año y duración de todas l
 print(listar_info(datos))
 
 print ("\n2. Contar información: Mostrar los títulos de las películas y el número de actores/actrices que tiene cada una. \n")
+print(contar_info(datos))
 
 print ("\n3. Buscar o filtrar información: Mostrar las películas que contengan en la sinopsis dos palabras dadas. \n")
 
