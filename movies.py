959ac1d53aa40957384ea2ff90b63e6b637ff660
Fran Madueño <47178991+franmadu6@users.noreply.github.com>
2019-04-09 09:52:46 +0200

    ejercicio 4

--- movies.py
+++ movies.py
@@ -14,7 +14,7 @@ def listar_info(datos):
         info = info + "Title: " + str(lista[listapelis]) + "\nAño: " + str(lista[listapelis + 1]) + "\nDuración: " + str(lista[listapelis + 2]).replace("PT","").replace("M"," Min.") + "\n\n"
     return info
 
-#2. Contar información: Mostrar los títulos de las películas y el número de actores/actrices que tiene cada una.s
+#2. Contar información: Mostrar los títulos de las películas y el número de actores/actrices que tiene cada una.
 def contar_info(datos):
     lista = []
     for actor in range(0,99):
@@ -41,6 +41,29 @@ def filtro_info(datos,p1,p2):
     for palabra in lista:
         cad = cad + "Película realionada -> " + palabra + "\n"
     return cad    
+#4. Buscar información relacionada: Mostrar las películas en las que ha trabajado un actor dado.
+def mostrar_actor(datos,actor):
+    lista=[]
+    for actuacion in range(0,99):
+        for actores in datos[actuacion]["actors"]:
+            if actores == actor: #para que no haya repeticiones de nombres.
+                lista.append(datos[actuacion]["title"])
+    
+    cad = actor + " ha trabjado en las siguientes peliculas: \n"
+    for actuacion in lista:
+        cad = cad + "Nombre: " + actuacion + "\n"
+    return cad
+
+def listar_actores(datos):
+    lista = []
+    for i in range(0,99):
+        lista.append(datos[i]["actors"])
+        
+    cad = "\nLista de actores: "
+    for lista_actores in range(0,len(lista),1):
+        cad = cad + "\nactores: " + str(lista[lista_actores])
+    return cad
+
     
 with open("movies.json","r") as fichero:
     datos = json.load(fichero)
@@ -59,5 +82,11 @@ p2 = str(input("Segunda palabra a buscar: "))
 print(filtro_info(datos,p1,p2))
 
 print ("\n4. Buscar información relacionada: Mostrar las películas en las que ha trabajado un actor dado. \n")
+input("\nPulse Enter para continuar.")
+print(listar_actores(datos))
+actor = str(input("Dime un actor: "))
+actor=actor.title()
+print(mostrar_actor(datos,actor))
 
 print ("\n5. Ejercicio libre: Mostrar el título y la url del póster de las tres películas con una media de puntuaciones más alta y lanzadas entre dos fechas dadas. \n")
+
